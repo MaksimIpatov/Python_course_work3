f6fe8e7b281aa09ab097143803a73952de4567d2
maksim ipatov <ipatovmaksim78@gmail.com>
2024-05-18 19:48:59 +0500

    Добавлены файлы.

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+
+@pytest.fixture
+def operations():
+    return [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": "31957.58",
+            "Column5": "руб.",
+            "Column6": "RUB",
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 41428829,
+            "state": "CANCELED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": "8221.37",
+            "Column5": "USD",
+            "Column6": "USD",
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+        },
+        {
+
+        },
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": "9824.07",
+            "Column5": "USD",
+            "Column6": "USD",
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+    ]
